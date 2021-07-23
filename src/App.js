6920705bce73a6c5579d60ad5595bf23b3af61c5
cashmy <cmyers880@gmail.com>
2021-07-23 07:32:44 -0500

    Final tweaks in code for initial presentation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const count = 0;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello dCC Class!</p>
+        <h3>Hello dCC Class!</h3>
+        <p>Let's Learn Redux Toolkit</p>
+
+        {/* Insert new code here */}
       </header>
     </div>
   );
