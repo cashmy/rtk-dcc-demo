9abe39d5fcc7742505ff261869a1c1359350d97d
cashmy <cmyers880@gmail.com>
2021-07-23 14:16:31 -0500

    Full code ready for demo.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// import { useSelector, useDispatch } from "react-redux";
+// import { incremented } from "./features/counter/counterSlice";
+import { Counter } from "./features/counter/Counter";
 
 function App() {
   const count = 0;
+  // const count = useSelector((state) => state.counter.value);
+  // const dispatch = useDispatch();
+
+  function handleClick() {
+    // dispatch(incremented());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +21,10 @@ function App() {
         <h3>Hello dCC Class!</h3>
         <p>Let's Learn Redux Toolkit</p>
 
-        {/* Insert new code here */}
+        <button type="button" onClick={handleClick}>
+          Count is: {count}
+        </button>
+        {/* <Counter /> */}
       </header>
     </div>
   );
